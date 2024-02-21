251116b06edaa419957ad6dcb480962cc47adaff
kade <zaccaro.kade@gmail.com>
2024-02-20 19:12:58 -0500

    Add loading spinner during API request

--- modules/getWeather.js
+++ modules/getWeather.js
@@ -3,9 +3,12 @@ import { prepareWeatherData } from "./prepareWeatherData.js";
 const form = document.querySelector(".location-form");
 const locationInput = document.querySelector(".location-input");
 const errorMsg = document.querySelector(".error-message");
+const loaderContainer = document.querySelector(".loader-container");
 
 export async function getWeather(unit, location) {
   try {
+    showLoadingAnimation();
+
     const response = await fetch("/.netlify/functions/fetchWeather", {
       method: "POST",
       headers: {
@@ -16,10 +19,21 @@ export async function getWeather(unit, location) {
 
     const weatherData = await response.json();
     prepareWeatherData(weatherData, unit);
+
     resetError();
   } catch (error) {
     handleError();
+  } finally {
+    hideLoadingAnimation();
   }
+}
+
+function showLoadingAnimation() {
+  loaderContainer.classList.remove("hide-loader");
+}
+
+function hideLoadingAnimation() {
+  loaderContainer.classList.add("hide-loader");
 }
 
 function handleError() {
